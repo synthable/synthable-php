7237d433ffe54b41cfd98535886c681392e089ca
Jason Knight <jason@synthable.com>
2011-05-08 15:53:34 -0700

    Change the burdon of deciding to create a new object or load one from the databse from the generated Base model to the library's BaseModel

--- lib/core/BaseModel.class.php
+++ lib/core/BaseModel.class.php
@@ -10,15 +10,26 @@ abstract class BaseModel {
 
     protected $hasErrors = false;
 
-    public function __construct($db) {
+    public function __construct($id, $db) {
         $this->db = $db;
+
+        /**
+         * If the id passed in is null, create a new object.
+         * Otherwise hydrate the object from the database
+         */
+        if($id === null) {
+            $this->setIsNew(true);
+            return true;
+        } else {
+            return $this->load($id);
+        }
     }
 
     protected function save() {
         if ($this->isValid() === false) {
             return false;
         }
-        
+
         if ($this->getIsNew()) {
             return $this->insert();
         } else {
